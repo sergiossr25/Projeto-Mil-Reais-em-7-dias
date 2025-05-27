f488b8d6450004d6a0e93a001c52bd042599f2e4
sergiossr25 <sergiodasilvarodrigues.srr@gmail.com>
2025-05-26 23:37:53 -0300

    Atualizações

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+
